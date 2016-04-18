2751cb5fbac68f9c422bb083e61e5333e456f3d4
egeorgel <edgar.georgel@gmail.com>
2016-04-18 18:47:54 +0200

    Ajout de test

--- src/test/java/com/adg/AppTest.java
+++ src/test/java/com/adg/AppTest.java
@@ -11,10 +11,13 @@ import static org.junit.Assert.assertEquals;
  * Unit test for simple App.
  */
 public class AppTest {
+
     @org.junit.Test
     public void testApp() {
         HomeController hc = new HomeController();
         String result = hc.home();
         assertEquals(result, "Coucou le monde");
     }
+
+
 }
