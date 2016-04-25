49026caafb618f882a5af385ce04a06b2ab1395c
Abdeldjalil <abdeldjalil.benkoussas@gmail.com>
2016-04-25 09:45:26 +0200

    update app.js

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,18 +1,22 @@
 
-// Sample static DATA pizza
 
-// Sample static DATA ingredients
-
-
+// Component <Pizza/> Contain List Of Pizzas
 var Pizza = React.createClass({
     getInitialState: function() {
         return {pizzas: []};
     },
     componentDidMount: function() {
-        this.serverRequest = $.get("http://localhost:8080/admin/pizza", function (result) {
-            this.setState({pizzas : result});
-            return pizzas;
-        }.bind(this));
+        $.ajax({
+            url : this.props.source,
+            datatype : 'json' ,
+            cache : false,
+            success : function (data) {
+                this.setState({pizzas : data});
+            }.bind(this),
+            error : function(xhr, status, err){
+                console.error(this.props.source, status, err.toString());
+            }.bind(this)
+        });
     },
     componentWillUnmount: function() {
         this.serverRequest.abort();
@@ -20,63 +24,80 @@ var Pizza = React.createClass({
     render: function() {
         // Generation of virtual DOM row pizza
         return <div>{this.state.pizzas.map(function (item, index) {
-            return (
-                <div className="col-md-4" key={index}>
-                <h3>{item.name}</h3>
+            return (<div className="col-md-4 text-center" key={index}>
+                <h2>{item.name}</h2>
                 <img src={item.url} className="imagepizza"/>
+                <div className="center-block"><button  className="btn btn-style">+</button> <button  className="btn btn-style">-</button></div>
 
             </div>)
         })}</div>;
 
     }
 });
-// <button className="btn btn-style">+</button>
-// <button className="btn btn-style">-</button>
-
+// Component <Ingredient/> Contain List Of Ingredients
 var Ingredients = React.createClass({
-    getInitialState : function(){
-        return  {ingredient : []};
+
+    getInitialState: function() {
+        return {ingredients: [], pizzas : []};
     },
     componentDidMount: function() {
-        this.serverRequest = $.get("http://localhost:8080/admin/ingredient", function (result) {
-            this.setState({ingredient : result});
-            return ingredient;
-        }.bind(this));
+        // Get pizzas
+        $.ajax({
+            url : this.props.source,
+            datatype : 'json' ,
+            cache : false,
+            success : function (data) {
+                this.setState({ingredients : data});
+            }.bind(this),
+            error : function(xhr, status, err){
+                console.error(this.props.source, status, err.toString());
+            }.bind(this)
+        });
+        // Get ingerdients
+        $.ajax({
+            url : this.props.source,
+            datatype : 'json' ,
+            cache : false,
+            success : function (data) {
+                this.setState({pizzas : data});
+            }.bind(this),
+            error : function(xhr, status, err){
+                console.error(this.props.source, status, err.toString());
+            }.bind(this)
+        });
     },
     componentWillUnmount: function() {
         this.serverRequest.abort();
     },
+
     render : function () {
-        return <ul >{this.state.ingredient.map(function (item, index) {
-            return (
-                <li key={index} class={item.name}>{item.name}</li>
-            )
-        })}</ul>
+
+        return<div> <form className="pizzaForm" onSubmit={this.handleSubmit}>
+        {this.state.ingredients.map(function (item, index) {
+            return <div key={item.name}  className="row">
+                <img src={item.url} className="img-ingredient col-md-1"/>
+                <div className="col-md-4 description">{item.name}</div>
+                <input  className="col-md-1" className="btn btn-style"/>
+                <button className="btn btn-style col-md-1 plus">+</button>
+                <button className="btn btn-style col-md-1 minus">-</button>
+            </div>
+        })}
+            <select className="col-md-3">{this.state.pizzas.map(function(item, index) {
+            return <option>{item.name}</option>;
+        })}</select></form></div>
     }
 
 });
 
 
-// <tr id="item1">
-//     <td id="name1">Mozarella
-//     </td>
-//     <td><input type="text" id="text" value="50" class="input-sizeAdmin" readonly="true" > </td>
-//     <td><button id="pl1" type="button" class="btn btn-style" value="+1" onClick="modifier(+1)">+</button> <button id="mn1" type="button" class="btn btn-style" value="-1" onClick="modifier(-1)">-</button>
-//     </td>
-//
-// </tr>
-
-
 ReactDOM.render(
 
-    <Pizza  items={pizzas} source="localhost:8080/pizza"/>,
+    <Pizza  items={pizzas} source="http://localhost:8080/admin/pizza"/>,
     document.getElementById('pizzas')
 );
 
-
-
 ReactDOM.render(
-    <Ingredients  items={ingredients} source="localhost:8080/ingredient" />,
+    <Ingredients   items={ingredients} source="http://localhost:8080/admin/ingredient"/>,
     document.getElementById('ingredients')
 
 );
