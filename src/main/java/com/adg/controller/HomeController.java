9731775a191ccf2d29a1695cfa20ced02bed0700
Abdeldjalil <abdeldjalil.benkoussas@gmail.com>
2016-04-27 10:28:28 +0200

    commit de rebase

--- src/main/java/com/adg/controller/HomeController.java
+++ src/main/java/com/adg/controller/HomeController.java
@@ -5,11 +5,19 @@ import com.adg.model.Ingredient;
 import com.adg.model.Pizza;
 import com.adg.service.JsonReader;
 import com.adg.service.PizzaService;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.comparator.BooleanComparator;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -32,10 +40,18 @@ public class HomeController {
         return pizzaService.getAllTypeOfIngredient();
     }
 
-    @RequestMapping(value = "pizza", method = RequestMethod.POST)
-    public boolean coockPizza(@RequestBody Pizza newpizza, @RequestBody List<Ingredient> listIngredient) {
-        Cooker cooker = new Cooker(newpizza, listIngredient);
-        return cooker.cookPizza();
+
+    @RequestMapping(value = "cooker", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public Boolean coockPizza(@RequestBody ObjectNode requete) {
+
+
+        System.out.println(requete.get("ingredient").getClass());
+
+
+        return false;
     }
+
 
 }
--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,11 +1,46 @@
-
-
+// ingredients list client-side
+var tmpIngredients = [];
 // Component <Pizza/> Contain List Of Pizzas
+var ElemPizza = React.createClass({
+    getInitialState : function () {
+        return {data: []};
+    },
+    render : function () {
+        return <div className="col-md-4 text-center">
+                 <h3>{this.props.name}</h3><img className="imagepizza" src={this.props.url}/>
+                <div className="center-block" ><button onClick={this.onChange.bind(this, this.props)} className="btn btn-style">+</button> <button  className="btn btn-style">-</button></div>
+        </div>
+    },
+    onChange : function () {
+        //this.setState({tmpIngredients : this.state.tmpIngredients.push(e.value)});
+        var pizza = {"name":"FIGUE - CHÈVRE","ingredients":[{"name":"Mozzarella","url":"http://pizza.dominos.fr/media/1078/Mozzarella.png"},{"name":"Oignons","url":"http://pizza.dominos.fr/media/1064/OIGNON.png"},{"name":"Base Crème Fraîche","url":"http://pizza.dominos.fr/media/1074/sauces.png"},{"name":"Fourme d\u0027Ambert","url":"http://pizza.dominos.fr/media/1050/fromages.png"},{"name":"Chèvre","url":"http://pizza.dominos.fr/images/lf.png"},{"name":"Origan","url":"http://pizza.dominos.fr/media/1054/herbes.png"},{"name":"Bacon","url":"http://pizza.dominos.fr/media/1039/bacon.png"},{"name":"Miel","url":"http://pizza.dominos.fr/images/lf.png"},{"name":"Figues Séchées","url":"http://pizza.dominos.fr/images/lf.png"}],"number":5,"url":"http://image.dominos.fr/images/pizza/PSFCdetail.png"};
+        var data = {"ingredient" :tmpIngredients, "pizza" : pizza};
+        console.log(this.props.target);
+        $.ajax({
+            url: this.props.target,
+            contentType:'application/json',
+            type: 'POST',
+            data:  JSON.stringify(data),
+            dataType : 'json',
+            success: function(data) {
+                //this.setState({data: data});
+                console.log(data);
+            }.bind(this),
+            error: function(xhr, status, err) {
+                //this.setState({data: data});
+                //console.error(this.props.url, status, err.toString());
+                console.log(data);
+            }.bind(this)
+        });
+    }
+
+});
 var Pizza = React.createClass({
     getInitialState: function() {
-        return {pizzas: []};
+        return {pizzas: [], count : 0};
     },
     componentDidMount: function() {
+
         $.ajax({
             url : this.props.source,
             datatype : 'json' ,
@@ -24,22 +59,45 @@ var Pizza = React.createClass({
     render: function() {
         // Generation of virtual DOM row pizza
         return <div>{this.state.pizzas.map(function (item, index) {
-            return (<div className="col-md-4 text-center" key={index}>
-                <h2>{item.name}</h2>
-                <img src={item.url} className="imagepizza"/>
-                <div className="center-block"><button  className="btn btn-style">+</button> <button  className="btn btn-style">-</button></div>
-
-            </div>)
+            return <ElemPizza name={item.name} url={item.url} target="http://localhost:8080/admin/cooker"/>
         })}</div>;
+    },
+    onChange(){
+        console.log("Hello ");
+    }
+});
 
+var ElemIngredient = React.createClass({
+
+    onChange : function (props) {
+        //this.setState({tmpIngredients : this.state.tmpIngredients.push(e.value)});
+
+        var ingredient = {};
+        ingredient.name = props.name;
+        ingredient.quantite += 1;
+        var result = $.grep(tmpIngredients, function(e){ return e.name == props.name });
+        // if ingredient does't exist in  tmpIngredient add it
+        if(result.length == 0 ){tmpIngredients = tmpIngredients.concat(ingredient); }
+        console.log(tmpIngredients);
+    },
+    render : function () {
+        return <div key={this.props.name}  className="row">
+            <img src={this.props.url} className="img-ingredient col-md-1"/>
+            <div    className="col-md-4 description">{this.props.name}</div>
+            <span className="quantite"></span>
+
+            <button onClick={this.onChange.bind(this, this.props)} className="btn btn-style col-md-1 plus" >+</button>
+            <button className="btn btn-style col-md-1 minus">-</button>
+        </div>
     }
 });
 // Component <Ingredient/> Contain List Of Ingredients
 var Ingredients = React.createClass({
 
     getInitialState: function() {
-        return {ingredients: [], pizzas : []};
+        return {ingredients: [], pizzas : [], tmpIngredients : []};
     },
+
     componentDidMount: function() {
         // Get pizzas
         $.ajax({
@@ -70,29 +128,24 @@ var Ingredients = React.createClass({
         this.serverRequest.abort();
     },
 
+
     render : function () {
 
-        return<div> <form className="pizzaForm" onSubmit={this.handleSubmit}>
+        return<div>
         {this.state.ingredients.map(function (item, index) {
-            return <div key={item.name}  className="row">
-                <img src={item.url} className="img-ingredient col-md-1"/>
-                <div className="col-md-4 description">{item.name}</div>
-                <input  className="col-md-1" className="btn btn-style"/>
-                <button className="btn btn-style col-md-1 plus">+</button>
-                <button className="btn btn-style col-md-1 minus">-</button>
-            </div>
-        })}
-            <select className="col-md-3">{this.state.pizzas.map(function(item, index) {
-            return <option>{item.name}</option>;
-        })}</select></form></div>
+            return <ElemIngredient name={item.name} url={item.url} />
+            })
+        }
+            <select className="col-md-5 list-pizzas">{this.state.pizzas.map(function(item) {
+            return <option key={item.name}>{item.name}</option>;
+        })}</select></div>
     }
 
 });
 
-
 ReactDOM.render(
 
-    <Pizza  items={pizzas} source="http://localhost:8080/admin/pizza"/>,
+    <Pizza  items={pizzas} source="http://localhost:8080/admin/pizza" />,
     document.getElementById('pizzas')
 );
 
@@ -101,5 +154,3 @@ ReactDOM.render(
     document.getElementById('ingredients')
 
 );
-
-
